3fe0d9e2fbd84202111285332327d8558e5a58a4
jojaeng2 <ds4ouj@naver.com>
2022-09-06 16:12:10 +0900

    main.py     정

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import feedparser, time
 URL="http://rss.blog.naver.com/ds4ouj" 
 RSS_FEED = feedparser.parse(URL)
 
-markdown_text = """
+markdown_text = '![image](https://user-images.githubusercontent.com/76645095/162124599-f9d701d6-e523-49c4-a6ce-193dc38f1026.png)'
+markdown_text += """
+
 ## ✅ Latest Blog Post
 
 """  # list of blog posts will be appended here
 
 MAX_POST = 5
-
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
