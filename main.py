9a76e5d0f7d8188c6167e69fa70ea7de21e3d3b8
jojaeng2 <ds4ouj@naver.com>
2022-12-06 20:17:42 +0900

    chore : markdown 문법 변경

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import feedparser, time
 URL="https://jojaeng2.tistory.com/rss" 
 RSS_FEED = feedparser.parse(URL)
 
+markdown_text = """
 
-markdown_text = '![image](https://user-images.githubusercontent.com/76645095/162124599-f9d701d6-e523-49c4-a6ce-193dc38f1026.png)'
-markdown_text += """
+markdown_text += ![image](https://user-images.githubusercontent.com/76645095/162124599-f9d701d6-e523-49c4-a6ce-193dc38f1026.png)
 
 ## ✅ Latest Blog Post
 
